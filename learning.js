e84aae8e278ad7d68e41fcee4556c744cfbd3f6b
iz.aia.h <imukisa024@gmail.com>
2022-12-30 11:58:38 +0300

    learning js

---
+++ learning.js
@@ -0,0 +1,5 @@
+document.body.innerHTML = "Heeeyyyy buddy";
+document.querySelector("p").innerHTML = "Testing";
+document.getElementById("tests").innerHTML = "Testing";
+document.getElementsByClassName("returns_arr")[0].innerHTML = "Testing";
+document.getElementsByTagName("returns_arr2")[1].innerHTML = "Testing";
