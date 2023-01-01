438ad3438b175e98739473803f736d75f13071a9
iz.aia.h <imukisa024@gmail.com>
2023-01-02 00:59:54 +0300

    learning modules

--- learning.js
+++ learning.js
@@ -1,5 +1,28 @@
-document.body.innerHTML = "Heeeyyyy buddy";
-document.querySelector("p").innerHTML = "Testing";
-document.getElementById("tests").innerHTML = "Testing";
-document.getElementsByClassName("returns_arr")[0].innerHTML = "Testing";
-document.getElementsByTagName("returns_arr2")[1].innerHTML = "Testing";
+// document.body.innerHTML = "Heeeyyyy buddy";
+// document.querySelector("p").innerHTML = "Testing";
+// document.getElementById("tests").innerHTML = "Testing";
+// document.getElementsByClassName("returns_arr")[0].innerHTML = "Testing";
+// document.getElementsByTagName("returns_arr2")[1].innerHTML = "Testing";
+
+function test(docElement) {
+  docElement.style.backgroundColor = "blue";
+}
+
+const toggleHiddenElement = (domElement) => {
+  if (domElement.style.display === "none") {
+    domElement.style.display = "block";
+  } else {
+    domElement.style.display = "none";
+  }
+};
+
+const resources = {
+  test,
+  toggleHiddenElement,
+};
+
+export default resources;
+// Or
+//  export { resources as default };
+
+// Check testing.js to see how we import!
---
+++ testimg.js
@@ -0,0 +1,3 @@
+import domFuntions from "./learning";
+// Or import { default as domFunctions } from '../modules/dom-functions.js';
+const { toggleHiddenElement, test } = domFuntions; // this is for when the default export is an object
