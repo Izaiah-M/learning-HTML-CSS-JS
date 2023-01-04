fd61caed388723d835872ef0bff51e2deb2a9160
iz.aia.h <imukisa024@gmail.com>
2023-01-04 12:02:46 +0300

    STO

--- learning.js
+++ learning.js
@@ -21,7 +21,15 @@ const resources = {
   toggleHiddenElement,
 };
 
-export default resources;
+console.log("I'm learning about");
+
+for (let idx = 0; idx < 999999999; idx++) {}
+
+// The second console.log() statement is
+// delayed by the for loop's execution
+console.log("the Event Loop");
+
+// export default resources;
 // Or
 //  export { resources as default };
 
